4632b904e83ee74ca1e17416bdf54d625d743b9b
yellowcurryninja <fardin5215@gmail.com>
2024-10-20 05:53:20 +0600

    make basic operator

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = function (firstNumber,secondNumber) {
+    return firstNumber+secondNumber
+}
+
+const subtract = function (firstNumber,secondNumber) {
+    return firstNumber-secondNumber
+}
+
+const multiply = function (firstNumber,secondNumber) {
+    return firstNumber*secondNumber
+}
+
+const divide = function (firstNumber,secondNumber) {
+    return firstNumber/secondNumber
+}
+
+let firstNumber = 0
+let operator = [add, subtract, multiply, divide]
+let secondNumber = 0
+
+let operate = function (operator, firstNumber,secondNumber ) {
+    return operator(firstNumber,secondNumber)
+}

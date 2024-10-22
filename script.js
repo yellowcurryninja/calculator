59b57aeaf854662e8bd912434e02682464a5a656
yellowcurryninja <fardin5215@gmail.com>
2024-10-22 11:15:55 +0600

    add eventlisteners to all buttons and add it to the display

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ let secondNumber = 0
 let operate = function (operator, firstNumber,secondNumber ) {
     return operator(firstNumber,secondNumber)
 }
+
+let display = document.querySelector(".display")
+let buttons = document.querySelector(".wrapper")
+let backspace = document.querySelector("#backspace")
+
+display.textContent = ''
+
+buttons.addEventListener("click", (e) => {
+    const isButton = e.target.nodeName === "BUTTON"
+    if (!isButton) {
+        return
+    }
+    display.textContent += e.target.textContent
+})
+
+backspace.addEventListener("click", () => {
+    display.textContent.slice(0,-1)
+})
+
+
+

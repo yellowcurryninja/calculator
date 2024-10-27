9632e728951aae214d93af26a47143d0f6a30ded
yellowcurryninja <fardin5215@gmail.com>
2024-10-27 07:10:17 +0600

    rewrite entire code, make it recognize firstNumber and secondNumber

--- script.js
+++ script.js
@@ -25,23 +25,96 @@ let operate = function (operator, firstNumber,secondNumber ) {
     return operator(firstNumber,secondNumber)
 }
 
+
 let display = document.querySelector(".display")
 let buttons = document.querySelector(".wrapper")
 let backspace = document.querySelector("#backspace")
+let test = document.querySelector(".test")
+let clear = document.querySelector("#clear")
+let remain = document.querySelector(".remainder")
+let dividation = document.querySelector(".divide")
+let into = document.querySelector(".multiply")
+let minus = document.querySelector(".minus")
+let plus = document.querySelector(".plus")
+let equals =  document.querySelector(".equals")
 
-display.textContent = ''
+// Numbers
 
-buttons.addEventListener("click", (e) => {
-    const isButton = e.target.nodeName === "BUTTON"
-    if (!isButton) {
-        return
-    }
-    display.textContent += e.target.textContent
+let one = document.querySelector(".one")
+let two = document.querySelector(".two")
+let three = document.querySelector(".three")
+let four = document.querySelector(".four")
+let five = document.querySelector(".five")
+let six = document.querySelector(".six")
+let seven = document.querySelector(".seven")
+let eight = document.querySelector(".eight")
+let nine = document.querySelector(".nine")
+let zero = document.querySelector(".zero")
+
+
+one.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
+})
+two.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
+})
+three.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
+})
+four.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
+})
+five.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
+})
+six.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
+})
+seven.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
+})
+eight.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
+})
+nine.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
+})
+zero.addEventListener("click", (e) => {
+    return display.textContent += e.target.textContent
 })
 
-backspace.addEventListener("click", () => {
-    display.textContent.slice(0,-1)
+// operators
+
+givenOperator = ""
+
+remain.addEventListener("click", (e) => {
+    givenOperator += e.target.textContent
+    return display.textContent += e.target.textContent
+})
+dividation.addEventListener("click", (e) => {
+    givenOperator += e.target.textContent
+    return display.textContent += e.target.textContent
+})
+into.addEventListener("click", (e) => {
+    givenOperator += e.target.textContent
+    return display.textContent += e.target.textContent
+})
+minus.addEventListener("click", (e) => {
+    givenOperator += e.target.textContent
+    return display.textContent += e.target.textContent
+})
+plus.addEventListener("click", (e) => {
+    givenOperator += e.target.textContent
+    return display.textContent += e.target.textContent
 })
 
+clear.addEventListener("click", () => {
+    return display.textContent = display.textContent.slice(-1,0)
+})
 
-
+equals.addEventListener("click", () => {
+    [firstNumber, secondNumber] = display.textContent.split(givenOperator)
+     console.log(firstNumber)
+     console.log(secondNumber)
+           
+})
